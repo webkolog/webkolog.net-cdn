edee148b59a92159ccc69e6963f47f65dbb3c4a8
Ali Cadan <webkolog@gmail.com>
2026-04-03 23:55:10 +0300

    Müzik oynatıcı kodlar eklendi, müzik dosyaları eklendi

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,41 @@ function createSummaryAndThumb(pID){
 	var summary = imgtag + '<div>' + removeHtmlTag(div.innerHTML,summ) + '</div>';
 	div.innerHTML = summary;
 }
+
+/* Musick Player */
+const audio = document.getElementById('main-audio');
+const playBtn = document.getElementById('play-pause-btn');
+const playIcon = playBtn.querySelector('i');
+const progressBar = document.getElementById('progress-bar');
+const progressContainer = document.getElementById('progress-container');
+
+// Oynat - Durdur
+playBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        playIcon.classList.replace('fa-play', 'fa-pause');
+    } else {
+        audio.pause();
+        playIcon.classList.replace('fa-pause', 'fa-play');
+    }
+});
+
+// İlerleme Çubuğunu Güncelle
+audio.addEventListener('timeupdate', () => {
+    const percent = (audio.currentTime / audio.duration) * 100;
+    progressBar.style.width = percent + '%';
+});
+
+// Çubuğa Tıklayarak İlerletme
+progressContainer.addEventListener('click', (e) => {
+    const width = progressContainer.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+});
+
+// Şarkı Bittiğinde Simgayi Değiştir
+audio.addEventListener('ended', () => {
+    playIcon.classList.replace('fa-pause', 'fa-play');
+    progressBar.style.width = '0%';
+});
